ea645a74d9728d079222934df0f0cd015d51818d
candymorgan <candyaz1000@gmail.com>
2023-02-06 15:59:03 -0800

    don

--- src/Components/AllComponents.jsx
+++ src/Components/AllComponents.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import Discover from "./Discover";
 import Navbar from "./Navbar";
 import Arts from "./Arts"
-// import { Display } from './Context';
+import Group7 from "../Group7.svg"
 
 function AllComponents() {
   return (
+    <div>
      <div className="container">
     <Navbar />
     <Discover />
     <Arts />
-    {/* <NextPage /> */}
+   
   * </div> 
+    <div className='image7'><img src={Group7} alt="" /></div>
+    </div>
   )
 }
 
